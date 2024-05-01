b475239e7c2261fd3b7c38cea4d9a129715b04a9
ZEFF <evandrogoetz5@gmail.com>
2024-05-01 11:28:30 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install curl perl unzip xtables-addons-common libtext-csv-xs-perl libmoosex-types-netaddr-ip-perl
+apt update -y
+apt upgrade -y
+apt install curl perl unzip xtables-addons-common libtext-csv-xs-perl libmoosex-types-netaddr-ip-perl
 
-sudo mkdir -p /usr/share/xt_geoip
-sudo wget -O /usr/local/bin/update-geoip.sh https://github.com/zeff0669/geoip_iptables/raw/main/update-geoip.sh
+mkdir -p /usr/share/xt_geoip
+wget -O /usr/local/bin/update-geoip.sh https://github.com/zeff0669/geoip_iptables/raw/main/update-geoip.sh
 
-sudo chmod +x /usr/local/bin/update-geoip.sh
+chmod +x /usr/local/bin/update-geoip.sh
 /usr/local/bin/update-geoip.sh
 
-sudo -O /usr/local/bin/iptables_br_only.sh wget https://github.com/zeff0669/geoip_iptables/raw/main/iptables_br_only.sh && bash /usr/local/bin/iptables_br_only.sh
+wget -O /usr/local/bin/iptables_br_only.sh  https://github.com/zeff0669/geoip_iptables/raw/main/iptables_br_only.sh && bash /usr/local/bin/iptables_br_only.sh
+
+if [[ ! -e /etc/rc.local ]]; then
+  echo "#!/bin/sh -e\n\nexit 0" >/etc/rc.local
+  chmod +x /etc/rc.local
+fi
+
+sed -i "1 a\bash /usr/local/bin/iptables_br_only.sh" /etc/rc.local
 
